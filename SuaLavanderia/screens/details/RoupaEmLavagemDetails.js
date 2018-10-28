7ba2c663d20d83ce2a27748b0c8d919887e901af
Elenilson Vieira <elenilsonvieira@gmail.com>
2018-10-28 11:14:38 -0300

    Primeira versão de roupaemlavagemdetails

--- SuaLavanderia/screens/details/RoupaEmLavagemDetails.js
+++ SuaLavanderia/screens/details/RoupaEmLavagemDetails.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {StyleSheet, View, ScrollView, Image, Text, TextInput, TouchableOpacity } from 'react-native';
+import {StyleSheet, View, Picker, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 
 export default class RoupaEmLavagemDetails extends React.Component {
 
     state ={
-        nome: '',
+        quantidade: '1',
+        soPassar:  false,
+        observacoes: '',
     };
 
     render(){
@@ -14,6 +16,39 @@ export default class RoupaEmLavagemDetails extends React.Component {
                     <TouchableOpacity onPress={() => this.props.navigation.goBack()} style={styles.button}>
                         <Image style={styles.icon} source={require('../../images/salvar_32x32.png')} />
                     </TouchableOpacity>
+                </View>
+
+                <View style={styles.unidadeContainer}>
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoTitle}>Quantidade: </Text>
+                            <TextInput
+                                style={styles.boxInput}
+                                autoFocus
+                                value={this.state.title}
+                                onChangeText={quantidade => this.setState({quantidade})}
+                            />
+                    </View>
+
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoTitle}>Só Passar?: </Text>
+                        <Picker
+                            style={styles.picker}
+                            selectecValue={this.state.soPassar}
+                            onValueChange={(itemValue, itemIndex) => this.setState({soPassar: itemValue})}>
+                            <Picker.Item label='Não' value={false} />
+                            <Picker.Item label='Sim' value={true} />
+                        </Picker>
+                    </View>
+
+                    <View style={styles.infoContainer}>
+                        <Text style={styles.infoTitle}>Observações: </Text>
+                        <TextInput
+                            style={styles.boxInput}
+                            placeholder="Observações"
+                            value={this.state.observacoes}
+                            onChangeText={observacoes => this.setState({observacoes})}
+                        />
+                    </View>
                 </View>
             </View>
         );
@@ -56,14 +91,15 @@ const styles = StyleSheet.create(
             margin: 20,
             justifyContent: 'center',
         },
-        lavagemInfoContainer: {
+        infoContainer: {
             flexDirection: 'row',
         },
         lavagemInfoContainerCliente: {
             alignItems: 'center',
         },
-        lavagemInfoTitle: {
+        infoTitle: {
             fontWeight: 'bold',
+            margin: 10,
         },
         lavagemInfo: {
         },
@@ -88,6 +124,12 @@ const styles = StyleSheet.create(
         roupasTitle: {
             fontSize: 18,
             fontWeight: 'bold',
+        },
+        picker:{
+            height: 40,
+            width: 100,
+            borderRadius: 15,
+            padding: 5,
         },
     }
 );
