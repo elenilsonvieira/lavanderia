1678c37210a4ae9799d13cb28c97d0c88c83b7fb
Elenilson Vieira <elenilsonvieira@gmail.com>
2018-12-16 23:15:13 -0300

    Melhorada a lavagemScreen

--- SuaLavanderia/screens/LavagemScreenCliente.js
+++ SuaLavanderia/screens/LavagemScreenCliente.js
@@ -313,7 +313,7 @@ const styles = StyleSheet.create(
           header:{
             alignItems: 'center',
             justifyContent: 'space-between',
-            height: 100,
+            height: 90,
             backgroundColor: '#FFF',
             flexDirection: 'row',
           },
