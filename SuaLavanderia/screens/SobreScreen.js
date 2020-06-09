182d61dd5452edb4aa5ef5857012d924f689f0e3
elenilsonvieira <elenilsonvieira@gmail.com>
2020-06-09 19:36:22 -0300

    Gerada a versão 20.6.9.1

--- SuaLavanderia/screens/SobreScreen.js
+++ SuaLavanderia/screens/SobreScreen.js
@@ -26,7 +26,7 @@ export default class SobreScreen extends Component {
       <View style={styles.container}>
         <Image source={require('../images/logo.png')} />
         <Text style={styles.welcome}>Aplicativo da Sua Lavanderia</Text>
-        <Text style={styles.instructions}>Versão 20.05.30.10</Text>
+        <Text style={styles.instructions}>Versão 20.06.9.1</Text>
       </View>
     );
   }
