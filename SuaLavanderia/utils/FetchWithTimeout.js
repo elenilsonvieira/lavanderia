ab81f77265b1aa9e45b54e6da2c082129910ebbc
elenilsonvieira <elenilsonvieira@gmail.com>
2020-06-25 16:24:10 -0300

    Criada a fetch com timeout e com 3 tentativas

--- SuaLavanderia/utils/FetchWithTimeout.js
+++ SuaLavanderia/utils/FetchWithTimeout.js
@@ -1,8 +1,20 @@
-export default function (url, options, timeout = 7000) {
+function fetchWithTimeout (url, options, timeout = 7000, i) {
     return Promise.race([
         fetch(url, options),
         new Promise((_, reject) =>
-            setTimeout(() => reject(new Error('timeout')), timeout)
+            setTimeout(() => reject(new Error('Timeout! Verifique a conexão com a internet. T=' + i)), timeout)
         )
     ]);
+}
+
+export default async function (url, options, timeout = 7000) {
+    try{
+        return await fetchWithTimeout(url, options, timeout, 1);
+    }catch(error1){
+        try{
+            return await fetchWithTimeout(url, options, timeout, 2);
+        }catch(error2){
+            return await fetchWithTimeout(url, options, timeout, 3);
+        }   
+    }
 }
