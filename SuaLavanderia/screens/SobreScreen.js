9ce9b9837e80e8890f0da34060021817a980461d
elenilsonvieira <elenilsonvieira@gmail.com>
2022-12-05 09:08:47 -0300

    Versão 22.12.5.1

--- SuaLavanderia/screens/SobreScreen.js
+++ SuaLavanderia/screens/SobreScreen.js
@@ -26,7 +26,7 @@ export default class SobreScreen extends Component {
       <View style={styles.container}>
         <Image source={require('../images/logo.png')} />
         <Text style={styles.welcome}>Aplicativo da Sua Lavanderia</Text>
-        <Text style={styles.instructions}>Versão 22.12.4.1</Text>
+        <Text style={styles.instructions}>Versão 22.12.5.1</Text>
         <Text style={styles.instructions}>@Copyright - Elenilson Vieira da Silva Filho</Text>
       </View>
     );
